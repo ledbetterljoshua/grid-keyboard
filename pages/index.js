ab0e1929d3bbf47545b5cbfaf888d88840d1e8cf
Joshua Ledbetter <ledbetterljoshua@gmail.com>
2020-05-23 16:04:42 -0500

    doing cool things

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Head from "next/head";
 import { Keyboard } from "../components/keyboard";
 export default function Home() {
   return (
-    <div style={{ width: 500 }} className="my-24 container mx-auto">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Keyboard />
+    <div className="text-sm md:text-md p-4">
+      <div style={{ maxWidth: 500 }} className="my-24 container mx-auto">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Keyboard />
+      </div>
     </div>
   );
 }
